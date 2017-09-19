67905916c98967374598090c41068c4b2ab70441
Lauren <lyeary@users.noreply.github.com>
2017-09-19 09:56:51 -0700

    updated functions for individual param trends

--- performanceLogGraphs.py
+++ performanceLogGraphs.py
@@ -1,27 +1,8 @@
-# Shows the trends for one athlete: sleep, hydration, nutrition, stress, fatigue, soreness, overall over time
-# Can configure the trends that show based on the input
-
 # SELECT userId, parameterId, UNIX_TIMESTAMP(recordedAt), intValue 
 # FROM userparameter 
 # WHERE userId in (4349) 
 # AND formquestionid in (103, 104, 105, 106, 107, 108, 109) 
 # LIMIT 100000;
-
-#  492 = fatigue
-#  493 = soreness
-#  494 = stress
-#  495 = sleep
-#  496 = nutrition
-#  497 = hydration
-#  498 = overall
-
-# "userid" : {
-#   "param1": {
-#     "dates": []
-#     "values": []
-#   }
-# }
-
 
 import csv
 import datetime
@@ -101,29 +82,9 @@ def parseDataFromCSV(filename):
         individualPerformanceLogData["overall"]["values"].append(int(row[3]))
 
 
-def timeseries(data, params, subject):
-
+def individualParamTrend(data, params, subject):
   # initialize the plot
   fig, ax = plt.subplots()
-
-  # if (params['formscore'] == 'true'):
-  #   graphTitle = "Individual (userId 4349) Form Score"
-  #   values = data.iloc[[7]]
-
-  #   ax.plot(dates, values, label='Form Score')
-
-  #   ax.grid(True)
-  #   ax.set_ylim(0, 38)
-  #   ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
-  #   fig.autofmt_xdate()
-  #   ax.legend(loc=3)
-
-  #   ## set axes and title
-  #   ax.set_xlabel('Dates')
-  #   ax.set_ylabel('Form Score (total)')
-  #   ax.set_title(graphTitle)
-  
-  
   graphTitle = "Individual (userId 4349) Trends for "
 
   if (params['fatigue'] == 'true'):
@@ -184,12 +145,29 @@ def timeseries(data, params, subject):
   plt.show()
 
 
-def graphRequest(trend, graphSubject):
+def athleteFormScore()
+  # if (params['formscore'] == 'true'):
+  #   graphTitle = "Individual (userId 4349) Form Score"
+  #   values = data.iloc[[7]]
 
+  #   ax.plot(dates, values, label='Form Score')
+
+  #   ax.grid(True)
+  #   ax.set_ylim(0, 38)
+  #   ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+  #   fig.autofmt_xdate()
+  #   ax.legend(loc=3)
+
+  #   ## set axes and title
+  #   ax.set_xlabel('Dates')
+  #   ax.set_ylabel('Form Score (total)')
+  #   ax.set_title(graphTitle)
+  
+
+def requestIndividualParamTrend():
   form_data = parseDataFromCSV('Individual_performance_log_data.csv')
 
   # initialize all to false
-  params = ''
   graphParams = {
     'sleep': 'false',
     'hydration': 'false',
@@ -201,38 +179,32 @@ def graphRequest(trend, graphSubject):
     'formscore': 'false'
   }
 
-  if (trend == 'param history'):
-    print
-    print("You can see trends for the following parameters: sleep, hydration, nutrition, stress, fatigue, soreness or overall.")
-    params = raw_input('Please input params separated by a comma: ')
+  print
+  print("You can see trends for the following parameters: sleep, hydration, nutrition, stress, fatigue, soreness or overall.")
+  params = raw_input('Please input params separated by a comma: ')
 
-    paramList = [p.strip() for p in params.split(',')]
-    
-    for param in paramList:
-      graphParams[param] = 'true'
-    
-  elif (trend == 'form score'):
-    graphParams.formscore = 'true'
+  paramList = [p.strip() for p in params.split(',')]
+  for param in paramList:
+    graphParams[param] = 'true'
   
-  timeseries(form_data, graphParams, graphSubject)
+  individualParamTrend(form_data, graphParams, graphSubject)
+
+def requestIndividualFormScore():
+
 
 
 def selectKPI():
   print("Available KPIs to view: ")
   print(" (A) Athlete Param History")
   print(" (B) Athlete Form Score")
-  # print(" (C) Team Param History")
-  # print(" (D) Team Form Score")
+
   KPI = raw_input("Please select one from the list above: ")
 
   if ((KPI == 'A') or (KPI == '(A)') or (KPI == 'a')):
-    graphRequest('param history', 'individual')
+    requestIndividualParamTrend()
   elif ((KPI == 'B') or (KPI == '(B)') or (KPI == 'b')):
-    graphRequest('form score', 'individual')
-  # elif ((KPI == 'C') or (KPI == '(C)') or (KPI == 'c')):
-  #   complianceRequest('param history', 'team')
-  # elif ((KPI == 'D') or (KPI == '(D)') or (KPI == 'd')):
-  #   complianceRequest('form score', 'team')
+    requestIndividualFormScore()
+
 
 
 
